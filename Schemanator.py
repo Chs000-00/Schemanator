25b147adcdc54eccee520f2449a0d59e42a90eb8
Igbalabanov <101612094+Chs000-00@users.noreply.github.com>
2024-10-04 14:00:24 -0700

    More stuff

--- Schemanator.py
+++ Schemanator.py
@@ -1,11 +1,9 @@
-class KeydValue():
+class KeyValue():
     def __init__(self, key, value):
         self.key: string = key
         super().__init__(key, value)
 
-
-
-class Sint(KeydValue):
+class KInt(KeyValue):
     def __init__(self, key, value):
         self.value: int = value
 
@@ -23,10 +21,56 @@ class Sint(KeydValue):
 
     def toYaml(self, depth):
         pass
-        
-class Sstring(KeydValue):
+
+class KString(KeyValue):
     def __init__(self, value):
         self.value: string = value
+
+    def toJson(self, depth):
+        pass
+
+    def toJson5(self, depth):
+        pass
+
+    def toXml(self, depth):
+        pass
+
+    def toToml(self, depth):
+        pass
+
+    def toYaml(self, depth):
+        pass
+
+
+class KList(KeyValue):
+    def __init__(self, value):
+        self.value: list = value
+
+    def toJson(self, depth):
+        out = "["
+        for o in self.value:
+            out.append(o.toJson(depth+1))
+
+        out.append("]")
+        return out
+            
+
+    def toJson5(self, depth):
+        pass
+
+    def toXml(self, depth):
+        pass
+
+    def toToml(self, depth):
+        pass
+
+    def toYaml(self, depth):
+        pass
+
+# TODO: Allow extending KDict to require values
+class KDict(KeyValue):
+    def __init__(self, value):
+        self.value: dict = value
 
     def toJson(self, depth):
         pass
